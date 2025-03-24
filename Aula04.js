6271925033ebc94fc8c015b10489e8d6bab22fe5
karoliny bispo <karol-bobio@hotmail.com>
2025-03-24 16:30:39 -0300

    operadores matematicos

---
+++ Aula04.js
@@ -0,0 +1,36 @@
+    //Operadores matematicos
+
+    let num1, num2, res
+    res = 10
+
+    num2 = 100; //uma maneira de declarar valor
+
+    num1=num2 = 40; // outra maneira de definir valor
+
+    num2 = 400
+
+    console.log(num1,num2);
+
+    let num3 = num1+num2;
+    console.log(num3)
+
+
+    //sabendo o resto da divisao
+    res = num2%12;
+    console.log(res)
+
+    // operador de incremento (adc mais 1)
+    num1++
+    console.log(num1)
+
+    //operador de decremento (diminui 1)
+    num2--
+    console.log(num2)
+
+    num1+=1 //eh o mesmo que num1 = num1 + 1, esta pegando 1 adc ao valor atual da variavel
+
+    num1-=1 // eh o mesmo que num1 = num1 - 1
+
+    num1+=10 // eh o mesmo que num1 = num1 + 10, esta pegando o valor da variavel e adc o valor 10
+
+    num1*=2 // mesmo coisa que as outra definicoes acima
