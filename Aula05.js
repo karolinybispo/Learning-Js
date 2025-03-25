4214eff035f77a56308ff0a2553677bf5fe2e532
karoliny bispo <karol-bobio@hotmail.com>
2025-03-24 21:27:38 -0300

    operadores relacionais

---
+++ Aula05.js
@@ -0,0 +1,22 @@
+
+    //Operadores ralacionais:
+
+    /*
+     > | >= | < | <== | == | = | !=
+    */
+
+    let num1 = 1, num2 = 20, num3 = 99
+
+    //isso ja eh uma operacao relacional. Esta comparando se o valor de num1 eh maior que num2 e vai retornar true ou false
+     console.log(num1 > num2)
+
+    // '>=' -> mum1 é maior ou igual a num3?
+    console.log(num1 >= num3)
+
+    // '!' inverte o valor ou operador de negacao. num1 e num2 sao diferentes, logo, o resultado deve ser false. Mas com o '!' vai resultar true
+    console.log(!(num1 == num2))
+
+    // != -> diferente
+    console.log(num1 != num3)
+
+
