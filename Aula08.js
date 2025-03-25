1dd7bd29d2ef99d8430efe3c127a157a0978c14f
karoliny bispo <karol-bobio@hotmail.com>
2025-03-25 19:24:44 -0300

    operadores Bitwise

---
+++ Aula08.js
@@ -0,0 +1,8 @@
+    /// Operadores Bitwise 
+
+    let n1 = 10
+    let n2 = 100
+
+    let res = n1 & n2
+
+    console.log(res) // ele esta retornando a soma da equivalencia dos binarios dos valores das variaveis n1 e n2
