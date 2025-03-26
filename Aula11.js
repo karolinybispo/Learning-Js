5a41e024b9017b97adc057245b5248676b526a55
karoliny bispo <karol-bobio@hotmail.com>
2025-03-26 13:28:56 -0300

    operador TypeOf

---
+++ Aula11.js
@@ -0,0 +1,17 @@
+    //Operador Typeof (retorna o tipo de dado da variavel)
+
+
+    let v1=10, v2="10", v3=true, v4 = {nome: "bruna"}
+
+    console.log(v1)
+    console.log(v2)
+    console.log(v3)
+    console.log(v4)
+    
+    console.log()
+
+    // usando typeof
+    console.log("valor " + v1 + "tipo: " + typeof(v1))
+    console.log("valor " + v2 + "tipo: " + typeof(v2))
+    console.log("valor " + v3 + "tipo: " + typeof(v3))
+    console.log("valor " + v4 + "tipo: " + typeof(v4))
